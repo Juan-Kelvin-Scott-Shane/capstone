da22c2d3afc344a22c3d09f0e5ac6ba72a929488
Kelvin <kelvin.daquon.wade@gmail.com>
2022-08-16 14:55:33 -0500

    update seeder files

---
+++ src/main/java/com/example/capstone/mysql/user.sql
@@ -0,0 +1,5 @@
+USE example_db;
+
+INSERT INTO users(city, description, email, embed_link, password, profile_img, state, username)
+VALUES('Warren','I’m a drummer, guitarist and song writer.Pretty much a bad ass','iplayitall@gmail.com','https://www.foofighters.com/','password',null,'Ohio','oldnirvanadude'),
+      ('Memphis','A versatile muti- instrumental music performer. I like to play a little bit of everything and enjoy just being on stage just making people smile and dance.','Guitargirl2002@gmail.com',null,'password',null,'Tennessee','AJguitagirl')
