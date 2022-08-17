d31adbcbe04548e91b5b05b781f3ac49b87c32a9
Scott Roberts <scott.w.roberts70@gmail.com>
2022-08-17 14:30:40 -0500

    added @Controller to the Homeand Authentication controllers

--- src/main/java/com/example/capstone/controllers/AuthenticationController.java
+++ src/main/java/com/example/capstone/controllers/AuthenticationController.java
@@ -1,7 +1,8 @@
 package com.example.capstone.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
