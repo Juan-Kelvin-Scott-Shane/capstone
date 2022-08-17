1007d4c9538ed85b893e0ddde867fbf965872201
Kelvin <kelvin.daquon.wade@gmail.com>
2022-08-17 14:19:46 -0500

    update

--- src/main/java/com/example/capstone/controllers/ProfileController.java
+++ src/main/java/com/example/capstone/controllers/ProfileController.java
@@ -1,18 +1,36 @@
 package com.example.capstone.controllers;
 
+import com.example.capstone.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProfileController {
+    private final UserRepository userDao;
+
+    public ProfileController(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+
+
     @RequestMapping (path = "/profile", method = RequestMethod.GET)
     public String viewProfile(){
         return "profile";
     }
-    @GetMapping("/login")
-    public String viewLogin(){
-        return "login";
+
+    @GetMapping("/profile/{id}/edit")
+    public String editProfile(Model model,@PathVariable long id){
+
+        return"profile";
+
     }
+
+
+
+
 }
--- src/main/java/com/example/capstone/controllers/UserController.java
+++ src/main/java/com/example/capstone/controllers/UserController.java
@@ -1,53 +1,55 @@
-//package com.example.capstone.controllers;
-//
-//import com.example.capstone.models.User;
-//import com.example.capstone.repositories.UserRepository;
+package com.example.capstone.controllers;
+
+import com.example.capstone.models.User;
+import com.example.capstone.repositories.UserRepository;
 //import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.PostMapping;
-//
-//public class UserController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
 //    private PasswordEncoder passwordEncoder;
-//    private UserRepository userDao;
-//
+    private UserRepository userDao;
+
 //    public UserController(PasswordEncoder passwordEncoder, UserRepository userRepository) {
 //        this.passwordEncoder = passwordEncoder;
 //        this.userDao = userRepository;
 //    }
-//
+
 //    public PasswordEncoder getPasswordEncoder() {
 //        return passwordEncoder;
 //    }
-//
+
 //    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
 //        this.passwordEncoder = passwordEncoder;
 //    }
-//
-//    public UserRepository getUserDao() {
-//        return userDao;
-//    }
-//
-//    public void setUserRepository(UserRepository userDao) {
-//        this.userDao = userDao;
-//    }
-//
-//    public UserController() {
-//    }
-//
-//    @GetMapping("/register")
-//    public String registrationForm(Model model){
-//        model.addAttribute("newUser",new User());
-//                return "register";
-//    }
+
+    public UserRepository getUserDao() {
+        return userDao;
+    }
+
+    public void setUserRepository(UserRepository userDao) {
+        this.userDao = userDao;
+    }
+
+    public UserController() {
+    }
+
+    @GetMapping("/register")
+    public String registrationForm(Model model){
+        model.addAttribute("newUser",new User());
+                return "registration";
+    }
 //    @PostMapping("/register")
 //    public String registration(@ModelAttribute User user){
 //        String hash =passwordEncoder.encode(user.getPassword());
 //        user.setPassword(hash);
 //        userDao.save(user);
 //        return "redirect:/profile";
-//
-//
+
+
 //    }
-//}
+}
