53279c588b09d541543a78824cc4504ffac19671
s-banks <shroud2@hotmail.com>
2022-08-21 11:03:29 -0500

    auth failure testing

--- src/main/java/com/example/capstone/config/SecurityConfiguration.java
+++ src/main/java/com/example/capstone/config/SecurityConfiguration.java
@@ -7,9 +7,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 
 @Configuration
@@ -33,6 +35,11 @@ public class SecurityConfiguration {
     }
 
     @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .formLogin()
@@ -51,6 +58,6 @@ public class SecurityConfiguration {
                 .antMatchers("/events/create")
                 .authenticated();
         return httpSecurity.build();
-
     }
+
 }
