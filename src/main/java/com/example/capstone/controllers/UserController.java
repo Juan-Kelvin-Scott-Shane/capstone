c4ffdb66a4374aba01076124be6d2430f03199e3
Shane Banks <shane.banks02@gmail.com>
2022-08-22 15:34:41 -0500

    Valid form data is repopulated when an error occurs and the page reloads

--- src/main/java/com/example/capstone/controllers/UserController.java
+++ src/main/java/com/example/capstone/controllers/UserController.java
@@ -11,10 +11,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.util.Objects;
 
 
 @Controller
@@ -44,21 +46,33 @@ public class UserController {
 	}
 
 	@PostMapping("/register")
-	public String processRegister(User user, HttpServletRequest request) throws UnsupportedEncodingException, MessagingException {
+	public String processRegister(User user, HttpServletRequest request, RedirectAttributes rm) throws UnsupportedEncodingException, MessagingException {
 		//Setup 3 variables to help check for error and existing data
 		boolean inputHasErrors = user.getUsername().isEmpty() || user.getEmail().isEmpty() || user.getPassword().isEmpty();
+		String passwordConfirmation = request.getParameter("verify-password");
 		User newUser = userDao.findByUsername(user.getUsername());
 		User uEmail = userDao.findByEmail(user.getEmail());
-		//run the first if to verify we aren't missing any data and that an existing user and/or email was not found
-		if (!inputHasErrors && newUser == null && uEmail == null) {
+		//run the first if to verify we aren't missing any data and that an existing user and/or email was not found and that the passwords match
+		if (!inputHasErrors && newUser == null && uEmail == null && (String.valueOf(user.getPassword()).equals(passwordConfirmation))) {
 			//runs user creation process in the UserServices service file
 			service.register(user, getSiteURL(request));
 			return "reg-conf";
+			//Else-if password/confirmed password do not match, redirect with a parameter
+		} else if (!String.valueOf(user.getPassword()).equals(passwordConfirmation)){
+			System.out.println(String.valueOf(user.getUsername()));
+			//set username & email attribute in session so that the fields repopulate on load and redirect
+			rm.addFlashAttribute("uName", String.valueOf(user.getUsername()));
+			rm.addFlashAttribute("email", String.valueOf(user.getEmail()));
+			return "redirect:/register?pmfail";
 			//Else-if an existing user was found, redirect with a parameter
 		} else if (newUser != null) {
+			//set email attribute in session so that the email field repopulates on load and redirect
+			rm.addFlashAttribute("email", String.valueOf(user.getEmail()));
 			return "redirect:/register?uexists";
 			//else this handles the finding of an existing email. redirect with parameter
 		} else {
+			//set username attribute in session so that the username field repopulates on load and redirect
+			rm.addFlashAttribute("uName", String.valueOf(user.getUsername()));
 			return "redirect:/register?eexists";
 		}
 	}
