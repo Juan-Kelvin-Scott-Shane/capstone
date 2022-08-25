91fae9ad60f9f253d2e5e1a7d7afdbf308a42473
Shane Banks <shane.banks02@gmail.com>
2022-08-25 15:54:45 -0500

    password reset working

--- src/main/java/com/example/capstone/config/SecurityConfiguration.java
+++ src/main/java/com/example/capstone/config/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -42,7 +43,7 @@ public class SecurityConfiguration {
                 .defaultSuccessUrl("/profile")
                 .permitAll()
                 .and()
-                .logout()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                 .logoutSuccessUrl("/login?logout")
                 .and()
                 .authorizeRequests()
