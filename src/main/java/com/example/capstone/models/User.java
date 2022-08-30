7d7fb0fda00e8cd630502349a2e95763069b5b5b
Shane Banks <shane.banks02@gmail.com>
2022-08-30 10:12:33 -0500

    dbfix

--- src/main/java/com/example/capstone/models/User.java
+++ src/main/java/com/example/capstone/models/User.java
@@ -232,15 +232,6 @@ public class User {
         this.instruments = instruments;
     }
 
-//    public List<Genre> getGenres() {
-//        return genres;
-//    }
-//
-//    public void setGenres(List<Genre> genres) {
-//        this.genres = genres;
-//
-//    }
-
     public List<Proficiency> getProficiencies() {
         return proficiencies;
     }
