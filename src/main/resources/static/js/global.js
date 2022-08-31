945062d4590eaa28eef54998609862a6ebfe71ee
Shane Banks <shane.banks02@gmail.com>
2022-08-30 19:50:07 -0500

    event images supported in card

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,3 +1,5 @@
+"use strict";
+(function () {
 
     let myInput = document.getElementById("password");
     let letter = document.getElementById("letter");
@@ -55,4 +57,24 @@
     length.classList.remove("valid");
     length.classList.add("invalid");
 }
-}
+
+    //file upload
+        function pickFile(){
+            let client = filestack.init(FILEPICKER_API_KEY);
+            client.pick({
+                //launching image picker and adding the restrictions of one file that is of an image file type
+                accept: 'image/*',
+                maxFiles: 1,
+            })
+                //Taking the results object in as 'result'
+                .then(function (result) {
+                    //Putting the returned file URL result in a string, and printing it to the console
+                    console.log(JSON.stringify(result.filesUploaded[0].url));
+                    let image = document.getElementById("output");
+                    let url = result.filesUploaded[0].url;
+
+                })
+        }
+    }
+
+}())
