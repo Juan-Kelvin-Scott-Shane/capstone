78da498695ff19048af346c0ac7f1f8502f6e2ca
Kelvin <kelvin.daquon.wade@gmail.com>
2022-09-01 17:08:31 -0500

    update

--- src/main/resources/static/js/instform.js
+++ src/main/resources/static/js/instform.js
@@ -16,8 +16,18 @@
  const btn = document.getElementById('addInst');
  btn.addEventListener('click', function () {
   let hidden = document.getElementsByClassName("instHide");
+  console.log(hidden);
   while (hidden.length)
    hidden[0].classList.remove("instHide");
+ })
+ const btn2 = document.getElementById("bio-edit")
+ btn2.addEventListener('click',function (){
+  const edit = document.getElementsByClassName("editable")
+  for(let i = 0; i<edit.length; i++) {
+   edit[i].removeAttribute("disabled")
+   edit[i].classList.remove("profile-input")
+
+  }
  })
 
 }())
