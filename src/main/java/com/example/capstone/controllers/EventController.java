985b8a6d04774637a50567d228f008f0488c9ff6
Kelvin <kelvin.daquon.wade@gmail.com>
2022-09-01 09:26:45 -0500

    update

--- src/main/java/com/example/capstone/controllers/EventController.java
+++ src/main/java/com/example/capstone/controllers/EventController.java
@@ -5,6 +5,8 @@ import com.example.capstone.models.User;
 import com.example.capstone.repositories.EventRepository;
 import com.example.capstone.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -26,37 +28,45 @@ public class EventController {
         this.eventDao = eventDao;
         this.userDao = userDao;
     }
-    @GetMapping("/events")
-    public String showEvent(Model model){
-        List<Event>all =  eventDao.findAll();
-        model.addAttribute("events", all);
-        return "all-events";
 
+    @GetMapping("/events")
+    public String showEvent(Model model) {
+        List<Event> all = eventDao.findAll();
+        model.addAttribute("events", all);
+        try {
+
+            User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            model.addAttribute("currentUser", currentUser);
+        } catch (Exception e) {
+            System.out.println("No User");
+        }
+        return "all-events";
     }
+
     @GetMapping("/event/show/{id}")
-    public String showEventId(@PathVariable long id, Model model){
-        model.addAttribute("event",eventDao.getById(id));
-        return"events/show";
+    public String showEventId(@PathVariable long id, Model model) {
+        model.addAttribute("event", eventDao.getById(id));
+        return "events/show";
     }
 
     @GetMapping("/events/create")
-    public String showCreationForm(Model model){
-        model.addAttribute("event",new Event());
+    public String showCreationForm(Model model) {
+        model.addAttribute("event", new Event());
         model.addAttribute("fileApi", fileApi);
         return "create-event";
     }
 
     @PostMapping("/events/create")
-    public String create(@ModelAttribute Event event, @RequestParam String date,@RequestParam String time) throws ParseException {
+    public String create(@ModelAttribute Event event, @RequestParam String date, @RequestParam String time) throws ParseException {
         String[] dateParts = date.split("-");
         String year = dateParts[0];
         String month = dateParts[1];
         String day = dateParts[2];
         String finalDate = String.format("%s/%s/%s", month, day, year);
-        String[]timeParts = time.split(":");
+        String[] timeParts = time.split(":");
         String hour = timeParts[0];
-        String minutes= timeParts[1];
-        String finalTime = String.format("%s:%s",hour,minutes);
+        String minutes = timeParts[1];
+        String finalTime = String.format("%s:%s", hour, minutes);
         event.setTime(finalTime);
         event.setDate(finalDate);
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -64,12 +74,13 @@ public class EventController {
         eventDao.save(event);
         return "redirect:/events";
     }
+
     @GetMapping("/events/{id}/edit")
-    public String editPost(Model model, @PathVariable long id){
+    public String editPost(Model model, @PathVariable long id) {
         Event event = eventDao.getById(id);
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if(currentUser.getId() == event.getOwner().getId()){
-            model.addAttribute("event",eventDao.getById(id));
+        if (currentUser.getId() == event.getOwner().getId()) {
+            model.addAttribute("event", eventDao.getById(id));
         }
         return "create-event";
     }
