aaded6c01fe22fb01a6176199425f0695596abf4
Kelvin <kelvin.daquon.wade@gmail.com>
2022-09-05 11:37:55 -0500

    update

--- src/main/java/com/example/capstone/repositories/ProficiencyRepository.java
+++ src/main/java/com/example/capstone/repositories/ProficiencyRepository.java
@@ -13,25 +13,6 @@ import java.util.List;
 public interface ProficiencyRepository extends JpaRepository<Proficiency,Long > {
 	Proficiency findByGenre(String id);
 	Proficiency findProficienciesByGenre(String id);
-	@Query("FROM Proficiency p where p.skill =?1")
-	List<Proficiency> findProficienciesSkillLIKE(@Param("title")String title);
-	Proficiency findProficienciesByGenre(Genre genre);
 
-	Proficiency findProficienciesByUserIdAndGenre(User user, Genre genre);
 
-//	@Query("From Proficiency p where p.genre.title")
-//			List<Proficiency> findProficienciesByGenreLike(@Param("title")String title);
-	Proficiency findProficienciesByGenreOrInstruments(Genre genre, Instrument instrument);
-	Proficiency findProficienciesByGenreAndInstruments(Genre genre, Instrument instrument);
-
-	@Query("From Proficiency p WHERE p.instruments.instName like %:name%")
-	List<Proficiency> findProficienciesByInstrumentsLike(@Param("name")String name);
-
-	@Query("From Proficiency p WHERE p.genre.title like %:title% AND p.instruments.instName LIKE %:name%")
-	List<Proficiency>findProficienciesByGenreTitleLikeAndInstrumentsInstName(@Param("title")String title, @Param("name")String name);
-
-	@Query ("From Proficiency p Where p.user.city Like %:city%")
-	List<Proficiency>findProficienciesByUserCityLike(@Param("city")String city);
-
-	Proficiency findByUserId(String id);
 }
