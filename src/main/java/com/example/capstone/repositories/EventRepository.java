b458c8e7c98d85caab91692677c7fc3fb1f5f023
Shane Banks <shane.banks02@gmail.com>
2022-09-06 09:55:57 -0500

    in  work

--- src/main/java/com/example/capstone/repositories/EventRepository.java
+++ src/main/java/com/example/capstone/repositories/EventRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface EventRepository extends JpaRepository<Event,Long> {
     Event deleteEventById(Long id);
     Event findEventByLocationAndDate(String location, String date);
-
 }
